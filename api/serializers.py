00d5bc8052d31143b6dbbc50d0c9bf73ddd6fab5
GoncaloNogueira1 <goncalo.p.c.nogueira@gmail.com>
2025-11-08 00:51:18 +0000

    basic app backend/frontend

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Hello
+from .models import * # import everything from models
 
-class HelloSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Hello
-        fields = ['id', 'message']
+        model = User
+        fields = ['id', 'username']
+        #... add more
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'director']
+        #... add more
